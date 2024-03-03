29eed0e955c6f785fa89c3f73e3728e66c56db4d
LucaDvc <lukadvc1@gmail.com>
2024-03-03 22:19:22 +0200

    created popular courses list component

---
+++ components/catalog/PopularCoursesList.jsx
@@ -0,0 +1,106 @@
+import {
+  Divider,
+  Icon,
+  List,
+  Spinner,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+  useTheme,
+} from '@ui-kitten/components';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import BasicCourseCard from '../course-cards/BasicCourseCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPopularCourses } from '../../features/catalog/catalogSlice';
+import { router } from 'expo-router';
+
+const renderHorizontalCourseItem = ({ item }) => (
+  <View style={{ flexDirection: 'column' }}>
+    <BasicCourseCard course={item.course1} />
+    <BasicCourseCard course={item.course2} />
+  </View>
+);
+
+const pairUpCourses = (courses) => {
+  const pairedCourses = [];
+  for (let i = 0; i < courses.length; i += 2) {
+    pairedCourses.push({
+      course1: courses[i],
+      course2: courses[i + 1] ? courses[i + 1] : null,
+    });
+  }
+  return pairedCourses;
+};
+
+const ForwardIcon = (props) => <Icon {...props} name='arrow-forward' />;
+
+const PopularCoursesList = () => {
+  const theme = useTheme();
+  const [pairedCourses, setPairedCourses] = React.useState([]);
+
+  const dispatch = useDispatch();
+  const { popularCourses, isError, isSuccess, isLoading, message } =
+    useSelector((state) => state.catalog.recommendedCourses);
+
+  React.useEffect(() => {
+    dispatch(getPopularCourses());
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    if (popularCourses && popularCourses.length > 0) {
+      setPairedCourses(pairUpCourses(popularCourses));
+    }
+  }, [popularCourses]);
+
+  return (
+    <>
+      <Divider />
+      <TopNavigation
+        title={() => <Text category='h6'>Popular Courses</Text>}
+        style={{
+          height: 36,
+          paddingHorizontal: 24,
+        }}
+        accessoryRight={
+          <TopNavigationAction
+            icon={ForwardIcon}
+            onPress={() => {
+              router.push('/catalog');
+            }}
+          />
+        }
+      />
+      {isLoading ? (
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 350,
+          }}
+        >
+          <Spinner size='giant' />
+        </View>
+      ) : (
+        <List
+          contentContainerStyle={styles.horizontalList}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={pairedCourses}
+          renderItem={renderHorizontalCourseItem}
+        />
+      )}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  horizontalList: {
+    marginVertical: 0,
+    paddingHorizontal: 8,
+    height: 350,
+    backgroundColor: '#FFF',
+  },
+});
+
+export default PopularCoursesList;
---
+++ components/course-cards/BasicCourseCard.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import {
+  Avatar,
+  Card,
+  Icon,
+  Layout,
+  Text,
+  useTheme,
+} from '@ui-kitten/components';
+import { StyleSheet, Platform } from 'react-native';
+import { router } from 'expo-router';
+
+const PeopleIcon = (props) => <Icon {...props} name='people' />;
+
+const StarIcon = (props) => <Icon {...props} name='star' />;
+
+const BasicCourseCard = ({ course }) => {
+  const theme = useTheme();
+  console.log('Rendering course:', course);
+
+  return course ? (
+    <Card
+      style={styles.card}
+      onPress={() => router.push(`(pages)/catalog/${course.id}`)}
+    >
+      <Layout style={{ display: 'flex', flexDirection: 'row' }}>
+        <Avatar
+          shape='rounded'
+          source={{ uri: course.image }}
+          size='giant'
+          style={{ borderRadius: 8 }}
+        />
+
+        <Layout
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            marginHorizontal: 12,
+            paddingRight: 24,
+          }}
+        >
+          <Text category='h6'>{course.title}</Text>
+
+          <Layout
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+            }}
+          >
+            <PeopleIcon
+              width={16}
+              height={16}
+              fill={theme['color-basic-600']}
+              style={{ marginRight: 2 }}
+            />
+            <Text category='p2' style={{ marginRight: 12 }}>
+              {course.enrolled_learners}
+            </Text>
+            {course.average_rating && (
+              <>
+                <StarIcon
+                  width={16}
+                  height={16}
+                  fill={theme['color-basic-600']}
+                  style={{ marginRight: 2 }}
+                />
+                <Text category='p2'>{course.average_rating}</Text>
+              </>
+            )}
+          </Layout>
+
+          <Text category='s1' numberOfLines={2} ellipsizeMode='tail'>
+            {course.intro}
+          </Text>
+        </Layout>
+      </Layout>
+
+      <Text
+        style={{
+          color:
+            course.price === '0'
+              ? theme['color-primary-600']
+              : theme['color-info-200'],
+        }}
+      >
+        {course.price === '0' ? 'Free' : '$' + course.price}
+      </Text>
+    </Card>
+  ) : null;
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: 360,
+    margin: 8,
+    borderRadius: 12,
+    elevation: 3,
+    // shadow properties for iOS
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3.84,
+      },
+    }),
+  },
+});
+
+export default BasicCourseCard;
