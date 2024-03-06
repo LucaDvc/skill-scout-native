4da1c268130c1b87581d375ef8a5917b885375a6
LucaDvc <lukadvc1@gmail.com>
2024-03-06 10:37:01 +0200

    register users reducer in redux store

--- features/store.js
+++ features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import catalogReducer from './catalog/catalogSlice';
+import usersReducer from './users/usersSlice';
 
 export const store = configureStore({
   reducer: {
     catalog: catalogReducer,
+    users: usersReducer,
   },
 });
