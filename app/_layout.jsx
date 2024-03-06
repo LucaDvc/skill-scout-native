95ca615184f1fe2c20ccbf08d8dffed1e44faa86
LucaDvc <lukadvc1@gmail.com>
2024-03-06 14:01:04 +0200

    calling initializeAuth on app init

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,12 +4,19 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { default as theme } from '../theme/theme.json';
 import { Stack } from 'expo-router';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from '../features/store';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { initializeAuth } from '../features/users/usersSlice';
 
-export default () => (
-  <Provider store={store}>
+const App = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(initializeAuth());
+  }, [dispatch, initializeAuth]);
+
+  return (
     <SafeAreaView style={{ flex: 1 }}>
       <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
         <IconRegistry icons={EvaIconsPack} />
@@ -26,8 +33,20 @@ export default () => (
               headerShown: false,
             }}
           />
+          <Stack.Screen
+            name='(users)'
+            options={{
+              headerShown: false,
+            }}
+          />
         </Stack>
       </ApplicationProvider>
     </SafeAreaView>
+  );
+};
+
+export default Index = () => (
+  <Provider store={store}>
+    <App />
   </Provider>
 );
